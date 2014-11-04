6776c1b12fdeeb17e9d372d488d4bc64741034c9
dvdrtrgn <dvdrtrgn@gmail.com>
2014-11-04 13:47:34 -0600

    reduce

--- scripts/main.js
+++ scripts/main.js
@@ -70,95 +70,41 @@ _CaptionTransitions["FLTTR|R"] = {
     }
 };
 
-var options1 = {
-    $AutoPlay: false,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
-    $DragOrientation: 3,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0),
+function makeOptions() {
+    return {
+        $AutoPlay: false,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
+        $DragOrientation: 3,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0),
 
-    $CaptionSliderOptions: {                            //[Optional] Options which specifies how to animate caption
-        $Class: $JssorCaptionSlider$,                   //[Required] Class to create instance to animate caption
-        $CaptionTransitions: _CaptionTransitions,       //[Required] An array of caption transitions to play caption, see caption transition section at jssor slideshow transition builder
-        $PlayInMode: 1,                                 //[Optional] 0 None (no play), 1 Chain (goes after main slide), 3 Chain Flatten (goes after main slide and flatten all caption animations), default value is 1
-        $PlayOutMode: 3                                 //[Optional] 0 None (no play), 1 Chain (goes before main slide), 3 Chain Flatten (goes before main slide and flatten all caption animations), default value is 1
-    },
+        $CaptionSliderOptions: {                            //[Optional] Options which specifies how to animate caption
+            $Class: $JssorCaptionSlider$,                   //[Required] Class to create instance to animate caption
+            $CaptionTransitions: _CaptionTransitions,       //[Required] An array of caption transitions to play caption, see caption transition section at jssor slideshow transition builder
+            $PlayInMode: 1,                                 //[Optional] 0 None (no play), 1 Chain (goes after main slide), 3 Chain Flatten (goes after main slide and flatten all caption animations), default value is 1
+            $PlayOutMode: 3                                 //[Optional] 0 None (no play), 1 Chain (goes before main slide), 3 Chain Flatten (goes before main slide and flatten all caption animations), default value is 1
+        },
 
-    $BulletNavigatorOptions: {                          //[Optional] Options to specify and enable navigator or not
-        $Class: $JssorBulletNavigator$,                 //[Required] Class to create navigator instance
-        $ChanceToShow: 0,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
-        $AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
-        $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
-        $Lanes: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
-        $SpacingX: 8,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
-        $SpacingY: 8,                                   //[Optional] Vertical space between each item in pixel, default value is 0
-        $Orientation: 1                                 //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
-    },
+        $BulletNavigatorOptions: {                          //[Optional] Options to specify and enable navigator or not
+            $Class: $JssorBulletNavigator$,                 //[Required] Class to create navigator instance
+            $ChanceToShow: 0,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+            $AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+            $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
+            $Lanes: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
+            $SpacingX: 8,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
+            $SpacingY: 8,                                   //[Optional] Vertical space between each item in pixel, default value is 0
+            $Orientation: 1                                 //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
+        },
 
-    $ArrowNavigatorOptions: {                           //[Optional] Options to specify and enable arrow navigator or not
-        $Class: $JssorArrowNavigator$,                  //[Requried] Class to create arrow navigator instance
-        $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
-        $AutoCenter: 2,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
-        $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
-    }
-};
+        $ArrowNavigatorOptions: {                           //[Optional] Options to specify and enable arrow navigator or not
+            $Class: $JssorArrowNavigator$,                  //[Requried] Class to create arrow navigator instance
+            $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+            $AutoCenter: 2,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+            $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
+        }
+    };
+}
+var options1 = makeOptions();
+var options2 = makeOptions();
+var options3 = makeOptions();
 
-var options2 = {
-    $AutoPlay: false,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
-    $DragOrientation: 3,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0),
-
-    $CaptionSliderOptions: {                            //[Optional] Options which specifies how to animate caption
-        $Class: $JssorCaptionSlider$,                   //[Required] Class to create instance to animate caption
-        $CaptionTransitions: _CaptionTransitions,       //[Required] An array of caption transitions to play caption, see caption transition section at jssor slideshow transition builder
-        $PlayInMode: 1,                                 //[Optional] 0 None (no play), 1 Chain (goes after main slide), 3 Chain Flatten (goes after main slide and flatten all caption animations), default value is 1
-        $PlayOutMode: 3                                 //[Optional] 0 None (no play), 1 Chain (goes before main slide), 3 Chain Flatten (goes before main slide and flatten all caption animations), default value is 1
-    },
-
-    $BulletNavigatorOptions: {                          //[Optional] Options to specify and enable navigator or not
-        $Class: $JssorBulletNavigator$,                 //[Required] Class to create navigator instance
-        $ChanceToShow: 0,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
-        $AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
-        $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
-        $Lanes: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
-        $SpacingX: 8,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
-        $SpacingY: 8,                                   //[Optional] Vertical space between each item in pixel, default value is 0
-        $Orientation: 1                                 //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
-    },
-
-    $ArrowNavigatorOptions: {                           //[Optional] Options to specify and enable arrow navigator or not
-        $Class: $JssorArrowNavigator$,                  //[Requried] Class to create arrow navigator instance
-        $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
-        $AutoCenter: 2,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
-        $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
-    }
-};
-
-var options3 = {
-    $AutoPlay: false,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
-    $DragOrientation: 3,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0),
-
-    $CaptionSliderOptions: {                            //[Optional] Options which specifies how to animate caption
-        $Class: $JssorCaptionSlider$,                   //[Required] Class to create instance to animate caption
-        $CaptionTransitions: _CaptionTransitions,       //[Required] An array of caption transitions to play caption, see caption transition section at jssor slideshow transition builder
-        $PlayInMode: 1,                                 //[Optional] 0 None (no play), 1 Chain (goes after main slide), 3 Chain Flatten (goes after main slide and flatten all caption animations), default value is 1
-        $PlayOutMode: 3                                 //[Optional] 0 None (no play), 1 Chain (goes before main slide), 3 Chain Flatten (goes before main slide and flatten all caption animations), default value is 1
-    },
-
-    $BulletNavigatorOptions: {                          //[Optional] Options to specify and enable navigator or not
-        $Class: $JssorBulletNavigator$,                 //[Required] Class to create navigator instance
-        $ChanceToShow: 0,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
-        $AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
-        $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
-        $Lanes: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
-        $SpacingX: 8,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
-        $SpacingY: 8,                                   //[Optional] Vertical space between each item in pixel, default value is 0
-        $Orientation: 1                                 //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
-    },
-
-    $ArrowNavigatorOptions: {                           //[Optional] Options to specify and enable arrow navigator or not
-        $Class: $JssorArrowNavigator$,                  //[Requried] Class to create arrow navigator instance
-        $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
-        $AutoCenter: 2,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
-        $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
-    }
-};
 
 window.jssor_slider1 = new $JssorSlider$("slider1_container", options1);
     window.jssor_slider2 = new $JssorSlider$("slider2_container", options2);
@@ -223,34 +169,26 @@ window.jssor_slider1 = new $JssorSlider$("slider1_container", options1);
         $(window).bind('resize', ScaleSlider);
     }
 
-
 //if (navigator.userAgent.match(/(iPhone|iPod|iPad)/)) {
 //    $(window).bind("orientationchange", ScaleSlider);
 //}
 //responsive code end
+
 });
 
 function scramble() {
-    var scramble1, scramble2, scramble3, i;
+    var rand, i;
 
-    scramble1 = Math.floor(Math.random() * 20) + 1;
-    scramble2 = Math.floor(Math.random() * 20) + 1;
-    scramble3 = Math.floor(Math.random() * 20) + 1;
+    rand = Math.random() * 9 + 1;
 
-    for (i=0; i<scramble1; i++) {
-        setTimeout(function() {
-            $("#btnRight1").click()
-            }, (i*20));
+    for (i = 0; i < rand; i++) {
+        window.setTimeout($.fn.click.bind($("#btnRight1")), i * 99);
     }
-    for (i=0; i<scramble2; i++) {
-        setTimeout(function() {
-            $("#btnLeft2").click()
-            }, (i*20));
+    for (i = 0; i < rand; i++) {
+        window.setTimeout($.fn.click.bind($("#btnLeft2")), i * 99);
     }
-    for (i=0; i<scramble3; i++) {
-        setTimeout(function() {
-            $("#btnRight3").click()
-            }, (i*20));
+    for (i = 0; i < rand; i++) {
+        window.setTimeout($.fn.click.bind($("#btnRight3")), i * 99);
     }
 }
 
@@ -263,29 +201,28 @@ function createShareLink() {
     currentSlideIndex2 = jssor_slider2.$CurrentIndex();
     currentSlideIndex3 = jssor_slider3.$CurrentIndex();
     shareLink = window.location.href + "?a=" + currentSlideIndex1 + "&b=" + currentSlideIndex2 + "&c=" + currentSlideIndex3;
+
     console.debug(shareLink);
-    document.getElementById("previewDiv").style.display="block";
-    document.getElementById("printCorners").style.display="block";
+    $("#previewDiv").show();
+    $("#printCorners").show();
 
     //duplicate snowman div
     original = document.getElementById('snowman');
-    destination = document.getElementById("previewDiv");
     clone = original.cloneNode(true); // "deep" clone
-    //
+
     clone.id = "snowman1";
-    destination.appendChild(clone);
+    $("#previewDiv").append(clone);
 }
 
 function closePreview() {
-    var snowman1 = document.getElementById("snowman1");
-
-    snowman1.parentNode.removeChild(snowman1);
-    document.getElementById("previewDiv").style.display="none";
-    document.getElementById("printCorners").style.display="none";
+    $("#snowman1").remove();
+    $("#previewDiv").hide();
+    $("#printCorners").hide();
 }
 
 function loadjscssfile(filename, filetype) {
     var fileref;
+
     if (filetype == "js") { //if filename is a external JavaScript file
         // alert('called');
         fileref = document.createElement('script');
