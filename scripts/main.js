3067e59625b287e936609bc81da1be7813b723e1
dvdrtrgn <dvdrtrgn@gmail.com>
2014-11-06 01:40:04 -0600

    refine Slides

--- scripts/main.js
+++ scripts/main.js
@@ -1,94 +1,61 @@
 /*jslint white:false */
-/*globals document, jQuery, window, $,
-    $JssorArrowNavigator$, $JssorBulletNavigator$, $JssorCaptionSlider$, $JssorEasing$, $JssorSlider$,
+/*globals document, jQuery, window,
+    getParameterByName, loadjscssfile, makeOptions,
+    $JssorEasing$,
+    $JssorSlider$,
 */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 var W = W || window,
     C = C || W.console,
-    Ss;
+    Slides;
 
-Ss = {
-    $: null,
+Slides = {
+    $: '.slider',
     A: null,
     B: null,
     C: null,
+    div: '.snowmen',
+    preview: '#Preview',
 };
 
-Ss.init = function () {
-    var self = Ss;
-    var qscheck, slideIndex1, slideIndex2, slideIndex3;
+Slides.init = function ($) {
+    var self = this,
+        div = $(self.div)[0],
+        preview$ = $(self.preview);
 
-    //Reference http://www.jssor.com/development/tip-make-responsive-slider.html
+    preview$.on('mouseup', function (evt) {
+        C.warn(evt);
+        if (evt.target === preview$[0]) {
+            evt.stopImmediatePropagation();
+            $('.nav .close').click();
+        }
+    });
 
-    qscheck = parseInt(getParameterByName('a'), 10);
-    slideIndex1 = parseInt(getParameterByName('a') || '0', 10);
-    slideIndex2 = parseInt(getParameterByName('b') || '0', 10);
-    slideIndex3 = parseInt(getParameterByName('c') || '0', 10);
+    self.$ = $(self.$).idem();
+    self.ia = parseInt(getParameterByName('a') || '0', 10);
+    self.ib = parseInt(getParameterByName('b') || '0', 10);
+    self.ic = parseInt(getParameterByName('c') || '0', 10);
 
-    if (qscheck >= 0) {
+    if (self.ia || self.ib || self.ic) {
         loadjscssfile('css/shared.css', 'css');
     } else {
         loadjscssfile('css/create.css', 'css');
     }
 
-    function makeOptions(x) {
-        return $.extend({
-            $StartIndex: 0,
-            $FillMode: 0,                                                   //  [Optional] The way to fill image in slide, 0 stretch, 1 contain (keep aspect ratio and put all inside slide), 2 cover (keep aspect ratio and cover whole slide), 4 actual size, 5 contain for large image, actual size for small image, default value is 0
-            $AutoPlay: false,                                               //  [Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
-            $DragOrientation: 1,                                            //  [Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0),
+    self.A = new $JssorSlider$(self.$[0].id, makeOptions({$StartIndex: self.ia}));
+    self.B = new $JssorSlider$(self.$[1].id, makeOptions({$StartIndex: self.ib}));
+    self.C = new $JssorSlider$(self.$[2].id, makeOptions({$StartIndex: self.ic}));
 
-            $CaptionSliderOptions: {                                        //  [Optional] Options which specifies how to animate caption
-                $Class: $JssorCaptionSlider$,                               //  [Required] Class to create instance to animate caption
-                $CaptionTransitions: [],                                    //  [Required] An array of caption transitions to play caption, see caption transition section at jssor slideshow transition builder
-                $PlayInMode: 1,                                             //  [Optional] 0 None (no play), 1 Chain (goes after main slide), 3 Chain Flatten (goes after main slide and flatten all caption animations), default value is 1
-                $PlayOutMode: 3                                             //  [Optional] 0 None (no play), 1 Chain (goes before main slide), 3 Chain Flatten (goes before main slide and flatten all caption animations), default value is 1
-            },
-
-            $BulletNavigatorOptions: {                                      //  [Optional] Options to specify and enable navigator or not
-                $Class: $JssorBulletNavigator$,                             //  [Required] Class to create navigator instance
-                $ChanceToShow: 0,                                           //  [Required] 0 Never, 1 Mouse Over, 2 Always
-                $AutoCenter: 1,                                             //  [Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
-                $Steps: 1,                                                  //  [Optional] Steps to go for each navigation request, default value is 1
-                $Lanes: 1,                                                  //  [Optional] Specify lanes to arrange items, default value is 1
-                $SpacingX: 8,                                               //  [Optional] Horizontal space between each item in pixel, default value is 0
-                $SpacingY: 8,                                               //  [Optional] Vertical space between each item in pixel, default value is 0
-                $Orientation: 1                                             //  [Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
-            },
-
-            $ArrowNavigatorOptions: {                                       //  [Optional] Options to specify and enable arrow navigator or not
-                $Class: $JssorArrowNavigator$,                              //  [Requried] Class to create arrow navigator instance
-                $ChanceToShow: 2,                                           //  [Required] 0 Never, 1 Mouse Over, 2 Always
-                $AutoCenter: 2,                                             //  [Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
-                $Steps: 1                                                   //  [Optional] Steps to go for each navigation request, default value is 1
-            }
-        }, x || {});
-    }
-
-    // EXTEND jquery
-    $.fn.idem = function () {
-        this.each(function (i, e) {
-            e.id = 'Random_' + (Math.random() * 1e9 | 0); // force an ID on it
-        });
-        return this;
-    };
-
-    Ss.$ = $('.slider').idem();
-    C.warn(Ss.$);
-
-    Ss.A = new $JssorSlider$(Ss.$[0].id, makeOptions({$StartIndex: slideIndex1}));
-    Ss.B = new $JssorSlider$(Ss.$[1].id, makeOptions({$StartIndex: slideIndex2}));
-    Ss.C = new $JssorSlider$(Ss.$[2].id, makeOptions({$StartIndex: slideIndex3}));
-
-    //responsive code begin
-    //you can remove responsive code if you don't want the slider scales while window resizes
+    // Reference http://www.jssor.com/development/tip-make-responsive-slider.html
+    //  responsive code begin
+    //  you can remove responsive code if you don't want the slider scales while window resizes
 
     function ScaleSlider() {
         var paddingWidth, minReserveWidth, parentElement, parentWidth, availableWidth, sliderWidth, clearFix;
 
         paddingWidth = 20; //                                                   reserve blank width for margin+padding: margin+padding-left (10) + margin+padding-right (10)
         minReserveWidth = 325; //                                               minimum width should reserve for text
-        parentElement = Ss.B.$Elmt.parentNode;
+        parentElement = self.B.$Elmt.parentNode;
         parentWidth = parentElement.clientWidth; //                             evaluate parent container width
 
         if (parentWidth) {
@@ -105,9 +72,9 @@ Ss.init = function () {
             }
 
             $('#clearFixDiv').css('clear', clearFix); //                        clear fix for safari 3.1, chrome 3
-            Ss.A.$ScaleWidth(sliderWidth);
-            Ss.B.$ScaleWidth(sliderWidth);
-            Ss.C.$ScaleWidth(sliderWidth);
+            self.A.$ScaleWidth(sliderWidth);
+            self.B.$ScaleWidth(sliderWidth);
+            self.C.$ScaleWidth(sliderWidth);
         } else {
             W.setTimeout(ScaleSlider, 30);
         }
@@ -128,52 +95,60 @@ Ss.init = function () {
         }
 
         for (i = 0; i < rando(); i++) {
-            W.setTimeout($.fn.click.bind(Ss.$.eq(0).find('.right')), i * 99);
+            W.setTimeout($.fn.click.bind(self.$.eq(0).find('.right')), i * 99);
         }
         for (i = 0; i < rando(); i++) {
-            W.setTimeout($.fn.click.bind(Ss.$.eq(1).find('.left')), i * 99);
+            W.setTimeout($.fn.click.bind(self.$.eq(1).find('.left')), i * 99);
         }
         for (i = 0; i < rando(); i++) {
-            W.setTimeout($.fn.click.bind(Ss.$.eq(2).find('.right')), i * 99);
+            W.setTimeout($.fn.click.bind(self.$.eq(2).find('.right')), i * 99);
         }
+        $('.button.preview').hide().fadeIn(3333);
     };
 
-    self.shareLink = function () {
-        var currentIndexes = [];
+    function readIndexes() {
+        self.ia = self.A.$CurrentIndex();
+        self.ib = self.B.$CurrentIndex();
+        self.ic = self.C.$CurrentIndex();
+    }
 
-        //generate link based of current slides positions
-        currentIndexes.push('?a=' + Ss.A.$CurrentIndex());
-        currentIndexes.push('&b=' + Ss.B.$CurrentIndex());
-        currentIndexes.push('&c=' + Ss.C.$CurrentIndex());
+    self.makeLink = function () {
+        var currentIndexes = []; // generate link based of current slides positions
+
+        readIndexes();
+        currentIndexes.push('?a=' + self.ia);
+        currentIndexes.push('&b=' + self.ib);
+        currentIndexes.push('&c=' + self.ic);
         return '' + W.location.href + currentIndexes.join('');
     };
 
-    self.createClone = function () {
-        var original, clone;
+    function makeClone() {
+        var clone;
 
-        $('#Preview').show();
-        //duplicate snowman div
-        original = $('.snowmen')[0];
-        clone = original.cloneNode(true); // 'deep' clone
-
-        clone.id = 'Clone';
-        $('#Preview').append(clone);
+        clone = div.cloneNode(true); // duplicate snowman
         $(clone).append($('.corners').clone());
+        clone.id = 'Clone';
 
-        W.alert(self.shareLink());
-    };
+        preview$.append(clone).show();
 
-    self.sharePreview = function () {
+        W.alert(self.makeLink());
+    }
+
+    self.openPreview = function () {
+        W.scrollTo(1,1);
+
         if (!$('#Clone').length) {
-            self.createClone();
+            makeClone();
         }
     };
 
     self.closePreview = function () {
         $('#Clone').remove();
-        $('#Preview').hide();
+        preview$.hide();
     };
 };
 
-$(Ss.init);
+jQuery(function () {
+    Slides.init(jQuery);
+});
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
