fb092ee0b9bab161be0f034aba24fb9f26332662
dvdrtrgn <dvdrtrgn@gmail.com>
2014-12-08 15:26:44 -0600

    patch up

--- libs/_util.js
+++ libs/_util.js
@@ -68,6 +68,125 @@ var Util = (function ($) { /// IIFE
     };
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+    /// MISC
+    U.slice = function (array, first, last) {
+        // 2 crazy things
+        if (W.isIE && !last) {
+            // fuckin IE cannot use undefined as a missing 2nd param
+            return array.slice(first);
+        } else {
+            // also the 2nd param cannot be zero (js has no neg-zero)
+            return array.slice(first, last || undefined);
+        }
+    };
+
+    U.Counter = (function () {
+        function Counter(n) {
+            n = (n || 0);
+            this.valueOf = this.toString = function () {
+                return n++;
+            };
+        }
+        return Counter;
+    }());
+
+    U.Cache = (function (obj) {
+        function cache() {}
+        cache.prototype = obj;
+        return cache;
+    }(new U.Counter()));
+
+    U.xyStore = function (ele, restore) {
+        var e = $(ele),
+        xy = e.position(),
+        dat = e.data(),
+        auto = {
+            left: 'auto',
+            top: 'auto'
+        };
+
+        if (restore) {
+            // either 1 restore or 2 reset to auto
+            e.css((restore < 2 && dat.xyStore) || auto);
+        } else {
+            dat.xyStore = xy;
+        }
+    };
+
+    U.abstractXY = function (evt) {
+        var o = {},
+        bs = $('body').scrollTop(),
+        hs = $('html').scrollTop(),
+        ie = $.browser.msie,
+        ff = $.browser.mozilla;
+
+        if (ie) {
+            o.x = evt.pageX;
+            o.y = evt.pageY + (bs);
+        } else {
+            o.x = evt.clientX;
+            o.y = evt.clientY + (ff ? hs : bs);
+        }
+        return o;
+    };
+
+    U.makeMailtos = function (ele) {
+        $('.email', (ele || 'body')) //
+        .removeClass('email') //
+        .addClass('mailto') //
+        .toMailto();
+    };
+
+    U.mfal = function () {
+        C.warn('U.mfal?');
+
+        var supplant = function (txt, cb) {
+            var me = $(this);
+            //
+            txt = '#' + (txt || me.attr('href'));
+            cb = cb || $.noop;
+            //
+            me.on('click', function (evt) {
+                evt.preventDefault();
+                if (typeof cb === 'function') {
+                    cb(this);
+                } else {
+                    W.location = cb;
+                }
+            }).attr('href', txt);
+        };
+        $('[href="."]').each(function () {
+            supplant.apply(this, ['reload', function () {
+                W.location.reload();
+            }]);
+        });
+        $('[href=".."]').each(function () {
+            supplant.apply(this, ['teamworks', 'http://portal.teamworks.wellsfargo.com/']);
+        });
+    };
+
+    U.query = function (nom) {
+        var A = W.location.search.slice(1).split('&'),
+            O = {};
+
+        $.each(A, function (i, e) {
+            var x = e.split('=');
+
+            O[x[0]] = x[1];
+        });
+        return nom ? (O[nom] || true) : O;
+    };
+
+    U.pathId = function () {
+        var path, tmp;
+
+        path = (' ' + W.location.pathname).split('/');
+        tmp = path.pop() || path.pop(); // trailing slash?
+        path = tmp.match('index.') ? path.pop() : tmp; // get directory?
+        return path.split('.').shift(); // remove any extension
+    };
+
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
     function _fixIE() {
         $('body').on('mouseover', '*', function (evt) {
@@ -82,6 +201,9 @@ var Util = (function ($) { /// IIFE
     void(W.isIE && _fixIE());
 
     $.extend(self, {
+        flatten: U.flatcat,
+        isDef: U.defined,
+        I: U.reflect,
         testrict: "eval('var x=0'),(typeof(x)!=='number'?'':'non-')+'strict'",
         fixIE: _fixIE,
     }, U);
--- libs/_xtnd/x-jquery.js
+++ libs/_xtnd/x-jquery.js
@@ -135,6 +135,53 @@
             me.html(me.html().replace(/\s+(\S+)\s*$/, '&nbsp;$1'));
         });
     };
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+    /// MISC
+    $.fn.toMailto = function () {
+        this.each(function () {
+            var me = $(this),
+                str, url;
+
+            str = me.text();
+            url = 'mailto:' + str.replace(' ', EMPT); // no spaces, please
+            me.text(EMPT);
+
+            str = str.replace(/(.+?)(\@.+)/, '$1' + ZWSP + '$2');
+            // wbr:after { content: "\00200B" } // &#x200b;
+            $('<a>').attr('href', url).html(str).appendTo(me);
+        });
+        return this;
+    };
+
+    $.scrollTo = function (ele, ms) {
+        var $me = $(ele),
+            doc = $.browser.msie ? 'html' : 'body';
+
+        if ($me.length) {
+            $(doc).stop().animate({
+                scrollTop: $me.offset().top,
+            }, ms);
+        }
+    };
+
+    $.ns = $.nameSpace = function (str, nom) {
+        var arr;
+
+        if (!nom) {
+            throw new Error('no namespace given');
+        }
+        arr = str.split(' ');
+
+        // add dot and name to each event type
+        str = _.map(arr, function (e) {
+            return e + '.' + nom;
+        }).join(' ');
+
+        if (!str) {
+            C.warn('namespace error');
+        }
+        return str;
+    };
 }(jQuery));
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 /*
